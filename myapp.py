4f7f9410659e90b837fb2b9435316711ef4fa3de
Immanuelboahen <immanuelboahen@gmail.com>
2017-07-17 15:24:20 +0000

    imma

--- myapp.py
+++ myapp.py
@@ -6,14 +6,6 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html", to="Sam Moorhouse")
 
-@app.route('/css/<path:path>')
-def send_css(path):
-    return send_from_directory('css', path)
-
-@app.route('/img/<path:path>')
-def send_img(path):
-    return send_from_directory('img', path)
-
 @app.route('/whereami')
 def whereami():
 	return "Kdua"
@@ -21,6 +13,3 @@ def whereami():
 @app.route('/hello/<name>')
 def foo(name):
     return render_template('index.html', to=name)
-
-if __name__ == '__main__':
-	app.run(host="0.0.0.0")
